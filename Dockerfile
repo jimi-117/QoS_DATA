01868c990e158f74e816bbef6aaddc11acbef0ea
jimi-117 <sho.jimi117@gmail.com>
2025-04-15 01:09:35 +0200

    feat:docker add dockerfile

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# QoS_AI/Dockerfile (他も同様の構造)
+FROM ghcr.io/astral-sh/uv:python3.12-bookworm-slim
+
+WORKDIR /app
+ENV UV_COMPILE_BYTECODE=1 UV_LINK_MODE=copy PYTHONUNBUFFERED=1
+
+RUN --mount=type=cache,target=/root/.cache/uv \
+    --mount=type=bind,source=uv.lock,target=uv.lock \
+    --mount=type=bind,source=pyproject.toml,target=pyproject.toml \
+    uv sync --frozen --no-install-project --no-dev
+
+
+ADD . /app
+RUN --mount=type=cache,target=/root/.cache/uv \
+    uv sync --frozen --no-dev
+
+ENV PATH="/app/.venv/bin:$PATH"
+ENTRYPOINT []
+
+# CMD ["fastapi", "dev", "--host", "0.0.0.0", "--port", "8001", "src.qos_data.main:app"]
--- app/main.py
+++ app/main.py
@@ -1,5 +1,15 @@
 from fastapi import FastAPI
 
+
+######### Settings for logging ########
+import os
+import logging
+
+
+
+
+
+
 app = FastAPI()
 
 @app.get("/")
@@ -9,3 +19,31 @@ async def root():
 @app.get("/health")
 async def health():
     return {"status": "healthy"}
+
+
+############################# TODO ############################
+@app.post("/create")
+async def create_dataset():
+    pass
+
+@app.get("/read")
+async def read_data():
+    pass
+
+@app.post("/upload")
+async def upload_data():
+    pass
+
+@app.post("/delete")
+async def delete_data():
+    pass
+
+##################################TODO, pour MLOps pipeline ##############
+
+@app.post("/loadDataset")
+async def load_dataset_from_clearml():
+    pass
+
+@app.post("/run_mlops")
+async def trigger_to_run_mlops():
+    pass
